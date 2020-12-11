c16e1e5f72b664eb9ed59044142408bc253872b0
DerLippo <32038450+DerLippo@users.noreply.github.com>
2020-12-11 16:16:10 +0100

    Standard 11.12.2020
    Stand null

--- inc/google/google_adsense.php
+++
@@ -1,1 +0,0 @@
-<script data-ad-client="ca-pub-8633778740771848" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
--- inc/google/google_api.php
+++
@@ -1,3 +0,0 @@
-<?php
-$apikey = 'AIzaSyBF_T039u2OejDsdho7sqGTa3kyHfwqSd8';
-?>
