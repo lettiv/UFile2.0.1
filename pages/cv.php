4a71514f964004f75434b9400cbcc5926680a3bc
DerLippo <32038450+DerLippo@users.noreply.github.com>
2020-12-21 11:39:13 +0100

    Update cv.php
    Eine kleine Basis wurde geschaffen

--- pages/cv.php
+++ pages/cv.php
@@ -8,10 +8,23 @@ else
 {
 require $_SERVER['DOCUMENT_ROOT']."/php/function/user/check_username.php";
 ?>
-<div class="container col-11">
+<div class="container col-11" style="margin-top:10px;">
     <div class="row">
-
+		<div class="container col-1">
+			<button type="submit">Back</button>
+		</div>
+		<div class="container col-2">
+			<h2>Classline</h2>
+		</div>
+		<div class="container col-8">
+			<div class="row">
+				
+			</div>
+		</div>	
     </div>
+	<div class="row">
+	Test
+	</div>
 </div>
 
 <?php
